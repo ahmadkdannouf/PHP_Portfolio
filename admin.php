3c0c4ed109c934580fefcc21e2fe4835c7b12a2e
Ahmad Dannouf <ahmadkdannouf@gmail.com>
2022-05-20 00:32:18 -0400

    Session may not be starting in time

--- admin.php
+++ admin.php
@@ -1,13 +1,19 @@
 <?php
 
 //require_once('database/connection.php');
-if(!isset($_SESSION['username'])):
-?>
+if(isset($_SESSION['username'])):?>
 
 <html>
 
 <h1>This is real this is me im exactly where im supposed to be</h1>
 <a class="button" href="login/logout.php">Log Out</a>
 </html>
+
+<?php else: ?>
+
+<?php header("Location:login/login.php");
+echo "hellos";
+?>
+
 
 <?php endif; ?>
